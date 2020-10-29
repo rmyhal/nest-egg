082c4758ddaf1d7d37ef0e6efc987a76c166920b
rmyhal <migalruslann@gmail.com>
2020-10-29 20:32:26 +0200

    create home fragment

--- app/src/main/java/com/rmyhal/nestegg/MainActivity.kt
+++ app/src/main/java/com/rmyhal/nestegg/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.rmyhal.nestegg
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rmyhal.nestegg.ui.home.HomeFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		if (savedInstanceState == null) {
+			supportFragmentManager.beginTransaction()
+				.replace(R.id.container, HomeFragment())
+				.commit()
+		}
 	}
 }
---
+++ app/src/main/java/com/rmyhal/nestegg/ui/home/HomeFragment.kt
@@ -0,0 +1,8 @@
+package com.rmyhal.nestegg.ui.home
+
+import androidx.fragment.app.Fragment
+import com.rmyhal.nestegg.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
