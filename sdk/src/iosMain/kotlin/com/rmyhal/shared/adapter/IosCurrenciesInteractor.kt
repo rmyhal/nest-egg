9fc88d914bb2886728226eee0a7b2725a793e652
rmyhal <migalruslann@gmail.com>
2021-01-19 13:07:24 +0200

    add suported currencies to ios interactor

--- sdk/src/iosMain/kotlin/com/rmyhal/shared/adapter/IosCurrenciesInteractor.kt
+++ sdk/src/iosMain/kotlin/com/rmyhal/shared/adapter/IosCurrenciesInteractor.kt
@@ -4,5 +4,7 @@ import com.rmyhal.shared.interactor.CurrenciesInteractor
 
 class IosCurrenciesInteractor(private val interactor: CurrenciesInteractor) {
 
+    val supportedCurrencies = interactor.supportedCurrencies
+
     fun getLastSelectedCurrencyCode() = interactor.getLastSelectedCurrencyCode()
 }
