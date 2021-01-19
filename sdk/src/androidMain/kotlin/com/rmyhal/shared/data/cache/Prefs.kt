4f6936ba8254b0a27e8f9739fd3b00b235319cf4
rmyhal <migalruslann@gmail.com>
2021-01-19 21:54:25 +0200

    replace removed preferenceKey

--- sdk/src/androidMain/kotlin/com/rmyhal/shared/data/cache/Prefs.kt
+++ sdk/src/androidMain/kotlin/com/rmyhal/shared/data/cache/Prefs.kt
@@ -2,7 +2,7 @@ package com.rmyhal.shared.data.cache
 
 import android.content.Context
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.preferencesKey
+import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.datastore.preferences.createDataStore
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.runBlocking
@@ -10,10 +10,11 @@ import kotlinx.coroutines.runBlocking
 internal actual class Prefs(context: Context) {
 
     companion object {
-        private val RATES_SYNC_TIME = preferencesKey<Long>("rates_sync_time")
+        private const val PREFS = "prefs"
+        private val RATES_SYNC_TIME = longPreferencesKey("rates_sync_time")
     }
 
-    private val preferences = context.createDataStore("prefs")
+    private val preferences = context.createDataStore(PREFS)
 
     actual var ratesSyncTime: Long
         get() = runBlocking { preferences.data.firstOrNull()?.get(RATES_SYNC_TIME) ?: -1 }
